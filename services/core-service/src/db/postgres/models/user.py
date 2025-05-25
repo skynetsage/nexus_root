e8e0aa79f6badf5305700e993ec848d0b4037e09
Jatin Sharma <jaters1200@gmail.com>
2025-05-25 12:16:25 +0530

    user table setup

---
+++ services/core-service/src/db/postgres/models/user.py
@@ -0,0 +1,22 @@
+from sqlalchemy import String, Integer, Boolean, TIMESTAMP, func, ARRAY
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from typing import Optional, List
+from datetime import datetime
+
+from ..engine import base
+
+class UserTable(base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, index=True)
+    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    roles: Mapped[List[str]] = mapped_column(ARRAY(String), default=list, nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now(), onupdate=func.now())
+
+    def __repr__(self) -> str:
+        return f"<UserTable(id={self.id}, username={self.username}, email={self.email}, verified={self.verified})>"
